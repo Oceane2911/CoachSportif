9d7f1689c846387af772c0e82a1687b8c9d9d8a1
Corenti Planet <corentinplanet@gmail.com>
2026-04-09 11:52:28 +0200

    Validation prestation

--- project/src/Controller/PrestationValidatorController.php
+++ project/src/Controller/PrestationValidatorController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Prestation;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\PrestationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,11 +12,23 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/validation')]
 final class PrestationValidatorController extends AbstractController
 {
-    #[Route(name: 'app_validation')]
+    #[Route('/', name: 'app_validation')]
     public function index(PrestationRepository $prestationRepository): Response
     {
         return $this->render('prestation_validator/index.html.twig', [
-            'prestations' => $prestationRepository->findAll()
+            'prestations' => $prestationRepository->findAll(),
         ]);
+    }
+
+    #[Route('/validate/{id}', name: 'app_prestation_validate', methods: ['POST'])]
+    public function validate(
+        Prestation $prestation,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $prestation->setIsValid(true);
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_validation');
     }
 }
