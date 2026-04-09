e6ead14a8b53a4a813961e944d27ab0113debe5b
Raphael TURCHI <raph@MacBook-Pro-de-Raphael.local>
2026-04-09 11:24:57 +0200

    liaison des differentes page

--- includes/header.php
+++ includes/header.php
@@ -17,7 +17,7 @@
             <ul>
                 <li><a href="index.php">accueil</a></li>
                 <li><a href="pages/documents.php">documents</a></li>
-                <li><a href="pages/contact.php">contact</a></li>
+                <li><a href="pages/formulaire.php">contact</a></li>
             </ul>
         </nav>
     </header>
